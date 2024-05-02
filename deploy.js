8855b95d62e9fa7cebf2d4050c2d02184be781a5
Shitanshu731 <shitanshu.731@gmail.com>
2024-05-02 12:22:39 +0530

    Create a transaction with Ganache

--- deploy.js
+++ deploy.js
@@ -1,12 +1,37 @@
-const ethers = requrie("ethers");
+//importing thers and JsonRpcProvider
+const ethers = require('ethers');
+const fs = require('fs-extra');
+async function main() {
+  // Json Rpc Provider - Connecting to local blockchain
+  const provider = new ethers.providers.JsonRpcProvider('http://172.29.176.1:7545'); // add your rpc server url from Ganache
 
-function main(){
-// compilr them in our code 
-// compile them seperately
-const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");// connection to the blockchain
-const wallet = new ethers.Wallet("0xcbb652adad001cad5685745ce3b1717591dc234119299137c9d931946af18f48",provider); //wallet Connection
+  // Connect to wallet to sign transactions
+  const wallet = new ethers.Wallet(
+    '0xd210f9d2cffc876501cd5d77bc90f07ac3a127bbbb0cd5855818bb94a8f3cf94',
+    provider
+  );
+
+  //Read ABI which you get post compilation using solc
+  const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8');
+  
+  //Read Binary which you get post compilation using solc
+  const binary = fs.readFileSync(
+    './SimpleStorage_sol_SimpleStorage.bin',
+    'utf8'
+  );
+
+  //Create Contract factory object to deploy
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log('Deploying, Please wait...');
+  
+  // Deploy and you are good to go
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
-main().then(() => process.exit(0)).catch((error) => {
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
     console.error(error);
-    process.exit(1)
-})
+    process.exit(1);
+  });
