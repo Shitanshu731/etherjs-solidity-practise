924a3b625d87de9aa72a9f0aba599a4d0f1e0896
Shitanshu731 <shitanshu.731@gmail.com>
2024-05-02 11:45:28 +0530

    Connect our contract to Ganache

--- deploy.js
+++ deploy.js
@@ -1,4 +1,12 @@
-function hi(){
-    console.log("hi");
+const ethers = requrie("ethers");
+
+function main(){
+// compilr them in our code 
+// compile them seperately
+const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");// connection to the blockchain
+const wallet = new ethers.Wallet("0xcbb652adad001cad5685745ce3b1717591dc234119299137c9d931946af18f48",provider); //wallet Connection
 }
-hi();
+main().then(() => process.exit(0)).catch((error) => {
+    console.error(error);
+    process.exit(1)
+})
